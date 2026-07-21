91e82b0f5dadde1119598aa340e719596185f489
shoizm <unsubscribetotakashi@gmail.com>
2026-07-21 21:46:00 +0800

    added dupli check, moved cmds to def

--- todolist.py
+++ todolist.py
@@ -14,33 +14,50 @@ ___________________________
 5. Exit
 ___________________________\n """)
 
-    action = int(input())
-    if action == 1:
-        task = input("\nEnter the task name: ")
-        tasks.append(task)
-        continue
+    def addTask():
+        task = input("\nEnter the task name: \n")
+        if task in tasks:
+            print("\n[ERROR] Task is already made!", file=sys.stderr)
+        else:
+            print("\nTask created.")
+            tasks.append(task)
 
-    elif action == 2:
+
+    def removeTask():
         delete = int(input("\nEnter task no. to delete\n"))
         calculated = delete -1
         del tasks[calculated]
         print("\nTask removed.")
+
+    
+    def viewTask():
+        for i, task in enumerate(tasks, start=1):
+            print("\nTask", "#" + str(i) + ":", "\n" + task)
+
+    def markTask():
+        print(tasks)
+
+    action = int(input("Choose an action: "))
+
+    if action == 1:
+        addTask()
+        continue
+
+    elif action == 2:
+        removeTask()
         continue
 
     elif action == 3:
-        for i, task in enumerate(tasks, start=1):
-            print("\n" + "Task:", i)
-            print(task)
-
+        viewTask()
+        continue
 
     elif action == 4:
+        markTask()
         print(tasks)
 
     elif action == 5:
-        print("Exiting To-Do-List")
+        print("Exiting Task-List")
         break
 
     else:
         print("\n[ERROR]", file=sys.stderr)
-
-    
