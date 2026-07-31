b60f0b86e5b5b485e86968e3c1729b2bab1bc37e
shoizm <unsubscribetotakashi@gmail.com>
2026-08-01 03:11:55 +0800

    finished

--- todolist.py
+++ todolist.py
@@ -1,6 +1,16 @@
+import pickle
 import sys
+from pathlib import Path
 
 tasks = []
+
+TASK = Path(__file__).parent / "tasks.deez"
+
+try:
+    with open(TASK, "rb") as f:
+        tasks = pickle.load(f)
+except FileNotFoundError, EOFError:
+    tasks = []
 
 while True:
     print("""
@@ -11,30 +21,32 @@ ___________________________
 2. Remove a task
 3. View all tasks
 4. Mark a task as completed
-5. Exit
+5. Exit & Save Changes
 ___________________________\n """)
 
     def addTask():
         task = input("\nEnter the task name: \n")
-        if any(yomom["name"] == task for yomom in tasks):
+        if any(taskname["name"] == task for taskname in tasks):
             print("\n[ERROR] Task is already made!", file=sys.stderr)
             return
-        tasks.append({
-            "name": task, 
-            "completed": False
-            })
+        tasks.append({"name": task, "completed": False})
         print("\nTask Created.")
         return
 
     def removeTask():
         viewTask()
-        delete = (int(input("\nEnter task No. to delete:\n"))-1)
-        if delete in tasks:
-            print("\nTask removed.")
-        else:
-            print("\n[ERROR] Could not find the given task", file=sys.stderr)
-            return
-        del tasks[delete]
+        
+        try:
+            taskdel = int(input("\nEnter task No. to delete:\n")) - 1
+            
+            if not (0 <= taskdel < len(tasks)):
+                print("\n[ERROR] Task not found.", file=sys.stderr)
+            else:
+                print("\nTask removed.")
+            
+            del tasks[taskdel]
+        except ValueError:
+            print("\n[ERROR] Incorrect Task Selection", file = sys.stderr)
 
     def viewTask():
         if not tasks:
@@ -44,12 +56,14 @@ ___________________________\n """)
         for i, task in enumerate(tasks, start=1):
             status = "/" if task["completed"] else "X"
             print(f"\nTask #{i} [{status}]: {task['name']}")
-            
+
     def markTask():
         if not tasks:
             print("\n[ERROR] No task found", file=sys.stderr)
             return
+        
         viewTask()
+        
         try:
             num = int(input("\nNumber of Task: "))
             if not (1 <= num <= len(tasks)):
@@ -58,7 +72,7 @@ ___________________________\n """)
             tasks[num - 1]["completed"] = True
             print("\nTask Completed.")
         except ValueError:
-                print("\n[ERROR] Enter a valid number.")
+            print("\n[ERROR] Enter a valid number.")
 
     try:
         action = int(input("Choose an action: "))
@@ -82,6 +96,8 @@ ___________________________\n """)
         continue
 
     elif action == 5:
-        print("\n[EXITING]")
+        print("\n[EXITING] Saving Changes")
+        
+        with open(TASK, "wb")as f:
+            pickle.dump(tasks, f)
         break
-
